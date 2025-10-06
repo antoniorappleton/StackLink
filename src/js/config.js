f506b9527bb81642f04e2b488ce57fd35847dca5
antoniorappleton <antonioappleton@gmail.com>
2025-10-06 22:54:17 +0100

    Estrutura da aplicação versão 1.1

--- src/js/config.js
+++ src/js/config.js
@@ -1,6 +1,5 @@
 // src/js/config.js
 export const COL = {
-  categories: 'categorias', // <- usa o nome que tens na base
-  categorias: 'categories',
+  categories: 'categories', // <- usa o nome que tens na base
   links: 'links'
 };
